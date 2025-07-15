41111363012509c1a456c5b9aa42557b691258a6
Karielly <kariellyycarvalho@gmail.com>
2025-07-15 16:43:03 -0300

    feat: Adiciona módulos de Empresas e Funcionários ao módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './core/database/database.module';
+import { EmpresasModule } from './modules/empresas/empresas.module';
+import { FuncionariosModule } from './modules/funcionarios/funcionarios.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, EmpresasModule, FuncionariosModule],
   controllers: [],
   providers: [],
 })
