8c430c226cfbb8f06e22e130397b9c8480d24817
Jean Carlos <jeancarllos99@outlook.com>
2025-08-22 19:03:35 -0300

    feat: Remove arquivo dev.db e atualiza dependências no package-lock.json

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -6,35 +6,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        const isProduction = configService.get<string>('NODE_ENV') === 'production';
-        
-        if (isProduction) {
-          return {
-            type: 'postgres',
-            host: configService.get<string>('DB_HOST'),
-            port: configService.get<number>('DB_PORT'),
-            username: configService.get<string>('DB_USERNAME'),
-            password: configService.get<string>('DB_PASSWORD'),
-            database: configService.get<string>('DB_DATABASE'),
-            entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
-            migrations: [__dirname + '/migrations/*{.ts,.js}'],
-            synchronize: true,
-            ssl: {
-              rejectUnauthorized: false,
-            },
-          };
-        }
-        
-        // Configuração para desenvolvimento com SQLite
-        return {
-          type: 'sqlite',
-          database: './dev.db',
-          entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
-          synchronize: true,
-          logging: true,
-        };
-      },
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
       inject: [ConfigService],
     }),
   ],
