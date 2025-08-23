0a23fb6920fb43d689efdbc77b68a811013518c8
Karielly <kariellyycarvalho@gmail.com>
2025-08-23 16:23:49 -0300

    feat: Atualiza configuração do TypeORM para incluir verificação de ambiente de produção no SSL

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -6,20 +6,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/migrations/*{.ts,.js}'],
-        synchronize: true,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }),
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+          entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/migrations/*{.ts,.js}'],
+          synchronize: true,
+          ssl:
+            configService.get<string>('NODE_ENV') === 'production'
+              ? {
+                  rejectUnauthorized: false,
+                }
+              : undefined,
+        };
+      },
       inject: [ConfigService],
     }),
   ],
